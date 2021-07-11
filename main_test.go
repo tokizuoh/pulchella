af954f7803327c39fb805e9ea808f5b2465e2214
tokizuoh <k_sakai@yumemi.co.jp>
2021-07-11 21:45:01 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,27 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestRemoveEmpty(t *testing.T) {
-	t.Error("force failure")
+	type args struct {
+		arr []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{name: "success", args: args{arr: []string{"1", "2", "", "3"}}, want: []string{"1", "2", "3"}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if res := removeEmpty(tt.args.arr); !reflect.DeepEqual(res, tt.want) {
+				t.Fatalf("removeEmpty() = %v, want %v", res, tt.want)
+			}
+		})
+	}
 }
