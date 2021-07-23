f7fca0f249d12c4b546596e2f5a7c5b035a16cf2
tokizuoh <k_sakai@yumemi.co.jp>
2021-07-23 15:07:25 +0900

    add processing get newest news id

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -261,7 +262,7 @@ func fetchEvent() {
 		log.Fatal("Error loading .env file")
 	}
 
-	targetURL := os.Getenv("TARGET_URL")
+	targetURL := os.Getenv("TARGET_URL_1")
 	for i := 595; i < 606; i++ {
 		id := strconv.Itoa(i)
 		url := targetURL + id
@@ -281,12 +282,73 @@ func fetchEvent() {
 	}
 }
 
+func getNewestID() (string, error) {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	targetURL := os.Getenv("TARGET_URL_2")
+
+	driver := agouti.ChromeDriver(agouti.ChromeOptions("args", []string{
+		"--headless",
+		"--window-size=1,1",
+		"--blink-settings=imagesEnabled=false",
+		"--disable-gpu",
+		"--disable-dev-shm-usage",
+		"no-sandbox",
+	}), agouti.Debug)
+
+	if err := driver.Start(); err != nil {
+		return "", err
+	}
+	defer driver.Stop()
+
+	page, err := driver.NewPage()
+	if err != nil {
+		return "", err
+	}
+
+	page.Navigate(targetURL)
+
+	src, err := page.HTML()
+	if err != nil {
+		return "", err
+	}
+
+	r := strings.NewReader(src)
+	doc, err := goquery.NewDocumentFromReader(r)
+	if err != nil {
+		return "", err
+	}
+
+	regex := regexp.MustCompile(`article.html\?id=\d{1,}`)
+	var needURL string
+	doc.Find("a").Each(func(_ int, s *goquery.Selection) {
+		if needURL != "" {
+			return
+		}
+		url, _ := s.Attr("href")
+		ok := regex.MatchString(url)
+		if ok {
+			needURL = url
+			return
+		}
+	})
+
+	if needURL == "" {
+		return "", fmt.Errorf("Not found href.")
+	}
+
+	return needURL, nil
+}
+
 func main() {
 	flag.Parse()
 
 	f := flag.Arg(0)
 	if f == "fetch" {
-		fetchEvent()
+		// getNewestID()
+		// fetchEvent()
 	} else {
 		// NOP
 	}
